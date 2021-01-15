5fdbd35c0aaebb2ba1fb1697ff9add4a62ab75b4
felipeturing <wfelipet@uni.pe>
2021-01-15 15:45:52 -0500

    test-6 finished

---
+++ src/test-6/test/4-max-min.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <omp.h>
+#include <time.h>
+
+#define CHUNKSIZE 25
+#define N 100
+
+using namespace std;
+
+int main(int argc, char **argv){
+    int n[N], i, chunk=CHUNKSIZE, n_max, n_min;
+
+    #pragma omp master
+    {
+        srand(time(NULL));
+        for (i=0; i<N; i++) n[i] = 1 + rand()%(99 + 1 - 1);
+        n_max = n[0];n_min = n[0];
+    }
+
+    #pragma omp parallel shared(n,n_max,n_min,chunk) private(i) num_threads(4)
+    {
+        #pragma omp for schedule(static, chunk) reduction(max:n_max) reduction(min:n_min) nowait
+        for (i = 0; i < N; i++) {
+            if(n[i] > n_max) n_max = n[i];
+            if(n[i] < n_min) n_min = n[i];
+        }
+        #pragma omp barrier
+        #pragma omp single
+        {
+            cout<<"El mín es: "<<n_min<<endl;
+            cout<<"El max es: "<<n_max<<endl;
+        }
+    }
+    return 0;
+}
--- src/test-6/test/4.cpp
+++ src/test-6/test/4.cpp
@@ -8,16 +8,16 @@ int main(int argc, char **argv){
     #pragma omp master
     a = 0;
 
-    #pragma omp parallel shared(a) num_threads(4) private(i)
+    #pragma omp parallel shared(a) private(i) num_threads(4)
     {
         #pragma omp for reduction(+:a) nowait
         for (i = 0; i < 10; i++) {
             a += i;
-            printf("%d, %d, %d\n",omp_get_thread_num(),i,a);
+            //printf("%d, %d, %d\n",omp_get_thread_num(),i,a);
         }
         #pragma omp barrier // Aquí debe ir la barrera puesto que necesite obtener el resultado de todos los hilos con el reduction para luego mostrar el resultado final, sino ejecuto esto no hay sincronización.
         #pragma omp single
             cout<<"La suma es: "<<a<<endl;
-    }s
+    }
     return 0;
 }
